78583ab221b586b6136f47d1425ba99e8f0ee6a9
Andreichirigut <andreichirigut07@gmail.com>
2020-01-24 11:10:19 +0100

    Se añadieron los métodos básicos

--- src/main/java/daw/andreiweb/Cliente.java
+++ src/main/java/daw/andreiweb/Cliente.java
@@ -11,6 +11,47 @@ package daw.andreiweb;
  */
 public class Cliente {
     private String nombre;
-    private String edad;
+    private int edad;
     private String correo;
+    
+    public Cliente(String nombre, int edad, String correo){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    public Cliente() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", edad=" + edad + ", correo=" + correo + '}';
+    }
+    
+    
 }
+
